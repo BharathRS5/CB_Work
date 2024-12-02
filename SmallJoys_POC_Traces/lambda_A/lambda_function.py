df58df903b3b2f1593c7ec2e4e5146ec8ced24ba
bharath_r <bharathr@cloudbuilders.io>
2024-12-02 23:17:07 +0530

    lambda

--- SmallJoys_POC_Traces/lambda_A/lambda_function.py
+++ SmallJoys_POC_Traces/lambda_A/lambda_function.py
@@ -3,7 +3,7 @@ import requests
 
 def lambda_handler(event, context):
     # URL of the API Gateway that triggers Lambda B
-    api_gateway_url = 'https://zamv2zuy9g.execute-api.us-east-1.amazonaws.com/dev'
+    api_gateway_url = 'https://zamv2zuy9g.execute-api.us-east-1.amazonaws.com/dev/triggerLambdaB'
 
     # Data that Lambda A will send to Lambda B
     data = {
