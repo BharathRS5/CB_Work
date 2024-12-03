1fd286e8f5ef2dbcccc096d5edc112484879a994
bharath_r <bharathr@cloudbuilders.io>
2024-12-03 13:29:02 +0530

    lambda

--- SmallJoys_POC_Traces/lambda_B/lambda_function.py
+++ SmallJoys_POC_Traces/lambda_B/lambda_function.py
@@ -1,10 +1,11 @@
 import json
 
 def lambda_handler(event, context):
-    # Lambda B simply receives the data from Lambda A
-    print(f"Received data: {event['message']}")
-    
+    # Lambda B processes the event from Lambda A via API Gateway 2
+    message = event['message']  # Extract message sent from Lambda A
+    print(f"Received message: {message}")  # You can check the logs in CloudWatch for this
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Lambda B processed the data successfully')
+        'body': json.dumps(f"Lambda B received the message: {message}")
     }
